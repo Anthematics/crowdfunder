0a54a2dbe0fb0ddb1b52dbb3c734ae14e6f375ce
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-18 18:20:39 -0400

    done fixing four test errors

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,13 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :not_owner_pledge
+
+def not_owner_pledge
+  if user == project.user
+    errors.add(:base, "project owner can't be pledger")
+  end
+end
+
+
 end
