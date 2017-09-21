f8c60a6587d107c2f9863036a0c545ad16cd11f1
Anthematics <jasonharder2@gmail.com>
2017-09-21 16:24:23 -0400

    added DateTime method to calculate time left to deadline in projects - shows in seconds will convert to proper (seconds/minutes/hours/days)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,15 @@
-class Project < ActiveRecord::Base
-  has_many :rewards
-  has_many :pledges
-  has_many :users, through: :pledges # backers
-  belongs_to :user # project owner
+class Project < ApplicationRecord
+	has_many :rewards
+	has_many :pledges
+	has_many :users, through: :pledges # backers
+	belongs_to :user # project owner
+	validates :title, :user, :description, :goal, :start_date, :end_date, presence: true
 
-  validates :title, :user, :description, :goal, :start_date, :end_date, presence: true
+
+	 def time_left_until_deadline
+		 current = DateTime.now
+		 @time_to_deadline = end_date - current
+		 return @time_to_deadline
+	 end
+
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,5 +1,5 @@
-class Reward < ActiveRecord::Base
-  belongs_to :project
-  validates :description, presence: true
-  validates :dollar_amount, presence: true
+class Reward < ApplicationRecord
+	belongs_to :project
+	validates :description, presence: true
+	validates :dollar_amount, presence: true
 end
