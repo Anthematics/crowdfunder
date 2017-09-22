94081e99bcee91dac2bf6ae0f6ba53ff73181e74
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-22 09:43:03 -0400

    made goal must be positive truthy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,24 @@
 class Project < ApplicationRecord
-	has_many :rewards
-	has_many :pledges
-	has_many :users, through: :pledges # backers
-	belongs_to :user # project owner
-	validates :title, :user, :description, :goal, :start_date, :end_date, presence: true
+  has_many :rewards
+  has_many :pledges
+  has_many :users, through: :pledges # backers
+  belongs_to :user # project owner
+  validates :title, :user, :description, :goal, :start_date, :end_date, presence: true
+  validate :present_start_date
+  validates :goal, numericality: {:greater_than => 0}
 
-	def time_left_until_deadline
-		current = DateTime.now
-		@time_to_deadline = end_date - current
-		return @time_to_deadline
-	end
+  def time_left_until_deadline
+    current = DateTime.now
+    @time_to_deadline = end_date - current
+    return @time_to_deadline
+  end
 
 
-	def present_start_date
-		if start_date < Date.today
-			errors.add(:start_date, "can't be in past")
-		end
-	end
+  def present_start_date
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in past")
+    end
+  end
 
 
 
